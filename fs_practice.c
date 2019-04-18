46dd89b835c3acc6c3e1f2a3ae6ceb54578da030
ricfield <ricfield@gmail.com>
2019-04-18 16:01:27 -0400

    Create fs_practice.c

---
+++ fs_practice.c
@@ -0,0 +1,21 @@
+// A simple filesystem
+
+
+int main(void)
+{
+   TOC *newFAT = NULL;
+   
+   // Let's hope we have the memory for this...
+   newFAT = (TOC*) malloc(sizeof(TOC));
+   if (newFAT == NULL)
+   {
+      return -1;
+   }
+   else
+   {
+      return 0;
+   }
+}
+   
+   
+   
