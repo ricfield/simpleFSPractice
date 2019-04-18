500f4bc0c51de3106a05391b0d3c3fc85af98129
ricfield <ricfield@gmail.com>
2019-04-18 16:07:51 -0400

    Update fs_structs.h

--- fs_structs.h
+++ fs_structs.h
@@ -1,18 +1,18 @@
 typedef struct tableOfContents
 {
-	fileHeader *files[20000]; 
+   fileHeader *files[20000]; 
 } TOC;
 
 typedef struct file
 {
-	char name[255];
-	fileChunk *startOfData;
+   char name[255];
+   fileChunk *startOfData;
 } fileHeader;
 
 typedef struct fileChunk
 {
-	int data;
-	fileChunk *prev;
-	fileChunk *next;
+   unsigned int data;
+   fileChunk *prev;
+   fileChunk *next;
 } chunk;
 
