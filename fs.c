6e17d7ab3da630a2d3b26bad66dbf8b6e04acdc9
ricfield <ricfield@gmail.com>
2019-04-22 16:55:35 -0400

    Update fs.c

--- fs.c
+++ fs.c
@@ -1,3 +1,6 @@
-void createFile(TOC *toc)
+//Create a simple file with one block of data
+void createSimpleFile(TOC *toc)
 {
+  toc->fileHeader[1].name = "aFile.txt";
+  toc->startOfData.data = 12345;
 }
